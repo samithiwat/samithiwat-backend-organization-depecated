a8085e660dd68aa73bbfb27f6bd3dd78d3b6d04f
samithiwat <samithiwat2544@gmail.com>
2022-05-06 11:58:17 +0700

    Implement relationships

--- src/model/organization.model.go
+++ src/model/organization.model.go
@@ -4,6 +4,8 @@ import "gorm.io/gorm"
 
 type Organization struct {
 	gorm.Model
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Teams       []*Team `json:"teams"`
+	Members     []*User `json:"members" gorm:"many2many:user_organization"`
 }
--- src/model/team.model.go
+++ src/model/team.model.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type Team struct {
 	gorm.Model
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	ParentID    uint    `json:"parent_id"`
-	SubTeams    []*Team `json:"sub_team" gorm:"foreignkey:ParentID"`
+	Name           string  `json:"name"`
+	Description    string  `json:"description"`
+	Members        []*User `json:"members" gorm:"many2many:user_team""`
+	ParentID       uint    `json:"parent_id"`
+	SubTeams       []*Team `json:"sub_team" gorm:"foreignkey:ParentID"`
+	OrganizationID uint    `json:"organization_id"`
 }
