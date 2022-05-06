148035d1c603ac34e5ee9460aa52a7d8434457a7
samithiwat <samithiwat2544@gmail.com>
2022-05-06 12:40:54 +0700

    Implement organization repository

--- src/repository/organization.repository.go
+++ src/repository/organization.repository.go
@@ -4,6 +4,7 @@ import (
 	"github.com/samithiwat/samithiwat-backend-organization/src/model"
 	"github.com/samithiwat/samithiwat-backend-organization/src/proto"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type OrganizationRepository struct {
@@ -14,22 +15,22 @@ func NewOrganizationRepository(db *gorm.DB) *OrganizationRepository {
 	return &OrganizationRepository{db: db}
 }
 
-func (r *OrganizationRepository) FindAll(*proto.PaginationMetadata, *[]*model.Organization) error {
-	return nil
+func (r *OrganizationRepository) FindAll(meta *proto.PaginationMetadata, perms *[]*model.Organization) error {
+	return r.db.Scopes(Pagination(perms, meta)).Find(&perms).Count(&meta.ItemCount).Error
 }
 
-func (r *OrganizationRepository) FindOne(uint, *model.Organization) error {
-	return nil
+func (r *OrganizationRepository) FindOne(id int, perm *model.Organization) error {
+	return r.db.Preload(clause.Associations).First(&perm, id).Error
 }
 
-func (r *OrganizationRepository) Create(*model.Organization) error {
-	return nil
+func (r *OrganizationRepository) Create(perm *model.Organization) error {
+	return r.db.Create(&perm).Error
 }
 
-func (r *OrganizationRepository) Update(uint, *model.Organization) error {
-	return nil
+func (r *OrganizationRepository) Update(id int, perm *model.Organization) error {
+	return r.db.Where(id).Updates(&perm).First(&perm).Error
 }
 
-func (r *OrganizationRepository) Delete(uint, *model.Organization) error {
-	return nil
+func (r *OrganizationRepository) Delete(id int, perm *model.Organization) error {
+	return r.db.First(&perm, id).Delete(&model.Organization{}).Error
 }
