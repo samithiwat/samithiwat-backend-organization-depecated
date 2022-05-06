ce317cdd8bdc8236b11efb20d346b57d55991b37
samithiwat <samithiwat2544@gmail.com>
2022-05-06 15:54:29 +0700

    Implement user service method

---
+++ src/service/user.service.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"context"
+	"github.com/samithiwat/samithiwat-backend-organization/src/proto"
+	"time"
+)
+
+type UserService struct {
+	client proto.UserServiceClient
+}
+
+func NewUserService(client proto.UserServiceClient) *UserService {
+	return &UserService{
+		client: client,
+	}
+}
+
+func (s *UserService) FindOne(id uint) (res *proto.UserResponse, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	res, err = s.client.FindOne(ctx, &proto.FindOneUserRequest{Id: int32(id)})
+	if err != nil {
+		res.Errors = append(res.Errors, err.Error())
+		return
+	}
+
+	return
+}
+
+func (s *UserService) FindMulti(ids []uint32) (res *proto.UserListResponse, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	res, err = s.client.FindMulti(ctx, &proto.FindMultiUserRequest{Ids: ids})
+	if err != nil {
+		res.Errors = append(res.Errors, err.Error())
+		return
+	}
+
+	return
+}
