0e166a4fe93a757f7700cd9bf13fcf3b03589cee
samithiwat <samithiwat2544@gmail.com>
2022-05-06 15:33:41 +0700

    Move client to service

--- src/service/role.service.go
+++ src/service/role.service.go
@@ -1,20 +1,43 @@
 package service
 
-import "github.com/samithiwat/samithiwat-backend-organization/src/proto"
-
-type RoleClient interface {
-	FindOne(*proto.Role) []error
-	Create(*proto.Role) []error
-	Update(*proto.Role) []error
-	Delete(*proto.Role) []error
-}
+import (
+	"context"
+	"github.com/samithiwat/samithiwat-backend-organization/src/proto"
+	"time"
+)
 
 type RoleService struct {
-	client *RoleClient
+	client proto.RoleServiceClient
 }
 
-func NewRoleService(client *RoleClient) *RoleService {
+func NewRoleService(client proto.RoleServiceClient) *RoleService {
 	return &RoleService{
 		client: client,
 	}
+}
+
+func (c *RoleService) FindOne(id uint) (res *proto.RoleResponse, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	res, err = c.client.FindOne(ctx, &proto.FindOneRoleRequest{Id: int32(id)})
+	if err != nil {
+		res.Errors = append(res.Errors, err.Error())
+		return
+	}
+
+	return
+}
+
+func (c *RoleService) FindMulti(ids []uint32) (res *proto.RoleListResponse, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	res, err = c.client.FindMulti(ctx, &proto.FindMultiRoleRequest{Ids: ids})
+	if err != nil {
+		res.Errors = append(res.Errors, err.Error())
+		return
+	}
+
+	return
 }
