1bc2ed9a5ef93770962a918323f97613252d6349
samithiwat <samithiwat2544@gmail.com>
2022-05-07 11:45:57 +0700

    Add foreign id to model

--- src/model/user.model.go
+++ src/model/user.model.go
@@ -6,6 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
+	UserID        uint            `json:"user_id" gorm:"index:,unique"`
 	Teams         []*Team         `json:"teams" gorm:"many2many:user_team"`
 	Organizations []*Organization `json:"organizations" gorm:"many2many:user_organization"`
 }
