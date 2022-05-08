a9f8cd42a0864b4d90777c6458f2ff4e34d3bd90
samithiwat <samithiwat2544@gmail.com>
2022-05-08 15:28:33 +0700

    Change foreign keys to pointer

--- src/model/contact.model.go
+++ src/model/contact.model.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Contact struct {
 	gorm.Model
-	ContactID      uint `json:"contact_id" gorm:"index:,unique"`
-	OrganizationID uint `json:"organization_id"`
+	ContactID      uint  `json:"contact_id" gorm:"index:,unique"`
+	OrganizationID *uint `json:"organization_id"`
 }
--- src/model/role.model.go
+++ src/model/role.model.go
@@ -6,6 +6,6 @@ import (
 
 type Role struct {
 	gorm.Model
-	RoleID         uint `json:"role_id" gorm:"index:,unique"`
-	OrganizationID uint `json:"organization_id"`
+	RoleID         uint  `json:"role_id" gorm:"index:,unique"`
+	OrganizationID *uint `json:"organization_id"`
 }
