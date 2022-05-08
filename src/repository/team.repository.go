2999e07ec8c76f9f02a840f79bf21fd79069e7f3
samithiwat <samithiwat2544@gmail.com>
2022-05-08 15:25:25 +0700

    Fix typo

--- src/repository/team.repository.go
+++ src/repository/team.repository.go
@@ -18,22 +18,22 @@ func (r *TeamRepository) FindAll(pagination *model.TeamPagination) error {
 	return r.db.Scopes(Pagination(&pagination.Items, &pagination.Meta, r.db)).Find(&pagination.Items).Count(&pagination.Meta.ItemCount).Error
 }
 
-func (r *TeamRepository) FindOne(id uint, perm *model.Team) error {
-	return r.db.Preload(clause.Associations).First(&perm, id).Error
+func (r *TeamRepository) FindOne(id uint, team *model.Team) error {
+	return r.db.Preload(clause.Associations).First(&team, id).Error
 }
 
 func (r *TeamRepository) FindMulti(ids []uint32, teams *[]*model.Team) error {
 	return r.db.Where("id IN ?", ids).Find(&teams).Error
 }
 
-func (r *TeamRepository) Create(perm *model.Team) error {
-	return r.db.Create(&perm).Error
+func (r *TeamRepository) Create(team *model.Team) error {
+	return r.db.Create(&team).Error
 }
 
-func (r *TeamRepository) Update(id uint, perm *model.Team) error {
-	return r.db.Where(id).Updates(&perm).First(&perm).Error
+func (r *TeamRepository) Update(id uint, team *model.Team) error {
+	return r.db.Where(id).Updates(&team).First(&team).Error
 }
 
-func (r *TeamRepository) Delete(id uint, perm *model.Team) error {
-	return r.db.First(&perm, id).Delete(&model.Team{}).Error
+func (r *TeamRepository) Delete(id uint, team *model.Team) error {
+	return r.db.First(&team, id).Delete(&model.Team{}).Error
 }
