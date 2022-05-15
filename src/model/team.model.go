470c3eeb23ec4c53b9fed49cbc33c45e02fe7293
samithiwat <samithiwat2544@gmail.com>
2022-05-15 22:17:58 +0700

    Remove unique constrain from team name

--- src/model/team.model.go
+++ src/model/team.model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Team struct {
 	gorm.Model
-	Name           string  `json:"name" gorm:"index:,unique"`
+	Name           string  `json:"name"`
 	Description    string  `json:"description"`
 	ParentID       *uint   `json:"parent_id"`
 	SubTeams       []*Team `json:"sub_team" gorm:"foreignkey:ParentID"`
